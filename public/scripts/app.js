52bbc15bb826bb6c574a31411d10d9e35fccda55
Teri <teri.eich@gmail.com>
2018-12-20 03:25:31 +0000

    added toggle functionality to form to add new rsvp record and form to edit rsvp; linked _rsvp-forms.scss into layout.scss

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,36 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+$(document).ready(function () {
+
+  // $(() => {
+  //   $.ajax({
+  //     method: "GET",
+  //     url: "/api/users"
+  //   }).done((users) => {
+  //     for(user of users) {
+  //       $("<div>").text(user.name).appendTo($("body"));
+  //     }
+  //   });;
+  // });
+
+//TOGGLE NEW RSVP FORM
+  $('#rsvp').click(function () {
+    let $newRSVP = $('.toggleRSVP');
+    if ($newRSVP.is(':hidden')) {
+      $newRSVP.slideDown();
+      $('#nameNew').focus();
+    } else {
+      $newRSVP.slideUp();
     }
-  });;
+  });
+
+  //TOGGLE EDIT RSVP FORM
+  $('#rsvpEdit').click(function () {
+    let $editRSVP = $('.toggleEdit');
+    if ($editRSVP.is(':hidden')) {
+      $editRSVP.slideDown();
+      $('nameEdit').focus();
+    } else {
+      $editRSVP.slideUp();
+    }
+  });
+
 });
