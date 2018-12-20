c3fd4ae584cb8a3e9e2d5d0d2a4c1e87df705156
RayCJ87 <shihcr.ke@gmail.com>
2018-12-20 18:53:57 +0000

    added endpoints and modified invite.ejs file

--- server.js
+++ server.js
@@ -13,20 +13,9 @@ const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
-// const pg          = require("pg");
-const settings = require("./settings");
+
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
-
-// Setup postgreSQL database
-// const client = new pg.Client({
-//   user     : settings.user,
-//   password : settings.password,
-//   database : settings.database,
-//   host     : settings.hostname,
-//   port     : settings.port,
-//   ssl      : settings.ssl
-// });
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -35,16 +24,6 @@ app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
-
-//// Connect to the database
-// client.connect((err) => {
-//   if (err) {
-//     return console.log("Connection Error", err);
-//   }
-
-// }
-
-
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -59,17 +38,6 @@ app.use(express.static("public"));
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
 
-//generate a new random string
-function generateRandomString() {
-  const randomKey = "1qaz2wsx3edc4rfv5tgb6yhn7ujm8ik9ol0pQAZWSXEDCRFVTGBYHNUJMIKOLP";
-  let output= "";
-  while (output.length < 6){
-    var tempNum = Math.floor(Math.random() * (randomKey.length));
-    output+= randomKey[tempNum];
-  }
-  return output;
-}
-
 // Home page
 app.get("/", (req, res) => {
   res.render("index");
@@ -77,22 +45,30 @@ app.get("/", (req, res) => {
 
 //organizer will be redirected after input personal information and description of event
 app.post("/", (req, res) => {
+
+
   res.redirect("/createEvent");
 });
 
 //organizer inputs attendee information and create event
 app.get("/createEvent", (req, res) => {
+  // const getNewEventURL = generateRandomString();
+
   res.render("invite");
 })
 
 //organizer will be redirected to an event page after an event is created
 app.post("/createEvent", (req, res) => {
-  res.redirect("/viewEvent");
+  res.redirect("/testEvent");
 })
 
 //read the event page
-app.get("/viewEvent", (req, res) => {
-  res.render("eventResponseForm");
+app.get("/testEvent", (req, res) => {
+  res.render("event_show");
+})
+
+app.post("/testEvent", (req, res) => {
+
 })
 
 
