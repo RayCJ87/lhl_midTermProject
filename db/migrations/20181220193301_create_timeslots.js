6d08e3686c01467c59a1945d7b6180f7e491c158
Teri <teri.eich@gmail.com>
2018-12-20 19:52:33 +0000

    added initial tables to db and some example seeds for testing

---
+++ db/migrations/20181220193301_create_timeslots.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('timeslots', function (table) {
+    table.increments('id').unsigned().notNullable();
+    table.dateTime('start_time');
+    table.dateTime('end_time').notNullable();
+    table.integer('event_id').unsigned().notNullable();
+
+    table.foreign('event_id').references('id').inTable('events');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('timeslots');
+};
