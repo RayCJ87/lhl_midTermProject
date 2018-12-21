37e774e4f13b95627263378f9918ee1226e83152
RayCJ87 <shihcr.ke@gmail.com>
2018-12-21 23:15:11 +0000

    hangling and sorting data inputed

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ const eventsRoutes = require("./routes/events")(DataHelpers);
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
-
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
