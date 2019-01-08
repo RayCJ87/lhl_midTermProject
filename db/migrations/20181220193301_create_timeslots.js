554f6f0a7b651c404fc0be747c6509ad4aff64ef
RayCJ87 <shihcr.ke@gmail.com>
2019-01-08 05:48:43 +0000

    commit before merge

--- db/migrations/20181220193301_create_timeslots.js
+++ db/migrations/20181220193301_create_timeslots.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('timeslots', function (table) {
     table.increments('id').unsigned().notNullable();
     table.dateTime('start_time');
-    table.dateTime('end_time').notNullable();
+
     table.integer('event_id').unsigned().notNullable();
 
     table.foreign('event_id').references('id').inTable('events');
